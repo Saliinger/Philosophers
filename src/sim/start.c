498751a45b870faa23cf61f5fdc1fb0cb1d2123e
Saliinger <alexisnoukan@gmail.com>
2024-08-11 16:03:07 +0200

    norm routine

--- src/sim/start.c
+++ src/sim/start.c
@@ -6,7 +6,7 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 13:53:33 by anoukan           #+#    #+#             */
-/*   Updated: 2024/08/11 15:58:45 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/08/11 16:03:00 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	start(t_data *data, t_philo **philo)
 	init_mutex(data, philo);
 	create_philo(philo, data);
 	thread(data, philo);
-	// ft_print(philo);
 	end(data, philo);
 }
+
+// ft_print(philo);
